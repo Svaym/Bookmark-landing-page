c40d6601350952dca8b70ea59fc44caa1cdaa938
Svaym <162036192+Svaym@users.noreply.github.com>
2024-04-27 14:48:57 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Header from "../components/Header/Header";
-import Intro from "../components/Intro/Intro";
-import Features from "../components/Features/Features";
-import Extension from "../components/Extension/Extension";
-import Questions from "../components/Questions/Questions";
-import Form from "../components/Form/Form";
-import Footer from "../components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Intro from "./components/Intro/Intro";
+import Features from "./components/Features/Features";
+import Extension from "./components/Extension/Extension";
+import Questions from "./components/Questions/Questions";
+import Form from "./components/Form/Form";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <>
